3deb443ca1671c2a3e45a764b35f23f969d1fe3e
sahildua96 <sahildua899@gmail.com>
2020-11-07 10:55:26 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const initialState = {
 
   onButtonSubmit = () => {
     this.setState({imageURL:this.state.input})
-    fetch('http://localhost:3000/imageUrl', {
+    fetch('https://afternoon-ravine-29774.herokuapp.com/imageUrl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body:JSON.stringify({
@@ -86,7 +86,7 @@ const initialState = {
         .then(response => response.json())
     .then(response =>{
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://afternoon-ravine-29774.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body:JSON.stringify({
